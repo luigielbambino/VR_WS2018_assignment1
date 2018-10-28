9f0356c6743b3b2e416f5180cdefbb9a712e547f
Muhammad Qumail <muhammad.qumail1@gmail.com>
2018-10-28 22:45:29 +0100

    1.2 finished
    Finished exercise 1.2 and added some text so the output is better readable

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from builtins import range
 import math
 
+
 class TMatrix:
     # width = 4
     # height = 4
@@ -34,8 +35,7 @@ class TMatrix:
 
 
 def make_trans_mat(x, y, z):
-    matrix_trans = [[1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [4, 8, 12, 16]]
-    trans = TMatrix(matrix_trans)
+    trans = TMatrix(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
     for i in range(len(trans.matrix)):
         for j in range(len(trans.matrix[i])):
             if j == 0:
@@ -57,9 +57,98 @@ def make_trans_mat(x, y, z):
                 print(trans.matrix[i][j])
 
 
-# def make_rot_mat(self, degree, axis):
+def make_rot_mat(degree, axis):
+    rot = TMatrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
 
-# def make_scale_mat(self, sx, sy, sz):
+    if axis == "x":
+        for i in range(len(rot.matrix)):
+            for j in range(len(rot.matrix[i])):
+                if i == 0:
+                    print(rot.matrix[i][j])
+                elif i == 1:
+                    if j == 1:
+                        print(math.cos(degree))
+                    elif j == 2:
+                        print(math.sin(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 2:
+                    if j == 1:
+                        print(math.sin(degree))
+                    elif j == 2:
+                        print(math.cos(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 3:
+                    print(rot.matrix[i][j])
+    elif axis == "y":
+        for i in range(len(rot.matrix)):
+            for j in range(len(rot.matrix[i])):
+                if i == 0:
+                    if j == 0:
+                        print(math.cos(degree))
+                    elif j == 2:
+                        print(-math.sin(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 1:
+                    print(rot.matrix[i][j])
+                elif i == 2:
+                    if j == 0:
+                        print(math.sin(degree))
+                    elif j == 2:
+                        print(math.cos(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 3:
+                    print(rot.matrix[i][j])
+    elif axis == "z":
+        for i in range(len(rot.matrix)):
+            for j in range(len(rot.matrix[i])):
+                if i == 0:
+                    if j == 0:
+                        print(math.cos(degree))
+                    elif j == 1:
+                        print(math.sin(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 1:
+                    if j == 0:
+                        print(-math.sin(degree))
+                    elif j == 1:
+                        print(math.cos(degree))
+                    else:
+                        print(rot.matrix[i][j])
+                elif i == 2:
+                    print(rot.matrix[i][j])
+                elif i == 3:
+                    print(rot.matrix[i][j])
+    else:
+        print("Invalid axis was entered. Please enter x,y or z. Case sensitive.")
+
+
+def make_scale_mat(sx, sy, sz):
+    scale = TMatrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
+
+    for i in range(len(scale.matrix)):
+        for j in range(len(scale.matrix[i])):
+            if i == 0:
+                if j == 0:
+                    print(scale.matrix[i][j] * sx)
+                else:
+                    print(scale.matrix[i][j])
+            elif i == 1:
+                if j == 1:
+                    print(scale.matrix[i][j] * sy)
+                else:
+                    print(scale.matrix[i][j])
+            elif i == 2:
+                if j == 2:
+                    print(scale.matrix[i][j] * sz)
+                else:
+                    print(scale.matrix[i][j])
+            elif i == 3:
+                print(scale.matrix[i][j])
 
 
 class Vector4:
@@ -67,13 +156,13 @@ class Vector4:
     def __init__(self, x=0, y=0, z=0, w=1):
         self.vector = [x, y, z, w]
         for i in range(len(self.vector)):
-            self.vector[i] = self.vector[i]/self.vector[len(self.vector)-1]
+            self.vector[i] = self.vector[i] / self.vector[len(self.vector) - 1]
 
 
 def euclidean_distance(p, v):
     # Function to calculate euclidean distance between two given vectors
     print("Euclidean distance: ")
-    distance = ((p[0]-v[0])**2 + (p[1]-v[1])**2 + (p[2]-v[2])**2)**.5
+    distance = ((p[0] - v[0]) ** 2 + (p[1] - v[1]) ** 2 + (p[2] - v[2]) ** 2) ** .5
     print(str(distance))
     return distance
 
@@ -86,10 +175,19 @@ def main():
 
     # Exercise 1.2
     make_trans_mat(1, 2, 3)
+    print("\nmake_rot_mat called with degree: 45 and axis: x")
+    make_rot_mat(45, 'x')
+    print("\nmake_rot_mat called with degree: 90 and axis: y")
+    make_rot_mat(90, 'y')
+    print("\nmake_rot_mat called with degree: 120 and axis: z")
+    make_rot_mat(120, 'z')
+    print("\nmake_scale_mat called with 1,2,3")
+    make_scale_mat(1, 2, 3)
 
     # Exercise 1.3
     v1 = Vector4(2, 4, 6, 2)
     v2 = Vector4(0, 0, 0, 1)
+    print("\n")
     ed = euclidean_distance(v1.vector, v2.vector)
 
     # Exercise 1.4
